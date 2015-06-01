45f405f230df4dfabf7da3d335ee1abf4b7d4a24
dhavalgala <dhaval.gala59@gmail.com>
2015-06-01 17:33:10 +0530

    geo location

--- js/controllers.js
+++ js/controllers.js
@@ -44,9 +44,9 @@ angular.module('starter.controllers', [])
     //    console.log($scope.testmsg.substring(index + 1, len));
     var successCallback = function (data, message) {
         $scope.data = data;
-//        var len = $scope.data.length;
-//        var index = $scope.data.indexOf(">");
-//        $scope.code = $scope.data.substring(index + 1, len);
+        //        var len = $scope.data.length;
+        //        var index = $scope.data.indexOf(">");
+        //        $scope.code = $scope.data.substring(index + 1, len);
         $scope.message = message;
         $scope.isfail = false;
     };
@@ -55,8 +55,53 @@ angular.module('starter.controllers', [])
         $scope.message = message;
         $scope.isfail = true;
     };
-    smsplugin.startReception(successCallback, failureCallback);
+    //smsplugin.startReception(successCallback, failureCallback);
     //    smsplugin.send('9029145077', "test", successCallback, failureCallback);
+
+
+    var onSuccess = function (position) {
+        console.log(position);
+        $scope.displayLocation(position.coords.latitude, position.coords.longitude);
+        //        alert('Latitude: ' + position.coords.latitude + '\n' +
+        //            'Longitude: ' + position.coords.longitude + '\n' +
+        //            'Altitude: ' + position.coords.altitude + '\n' +
+        //            'Accuracy: ' + position.coords.accuracy + '\n' +
+        //            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+        //            'Heading: ' + position.coords.heading + '\n' +
+        //            'Speed: ' + position.coords.speed + '\n' +
+        //            'Timestamp: ' + position.timestamp + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError,{ enableHighAccuracy: true });
+
+    $scope.displayLocation = function (latitude, longitude) {
+        var request = new XMLHttpRequest();
+
+        var method = 'GET';
+        var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
+        var async = true;
+
+        request.open(method, url, async);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                var data = JSON.parse(request.responseText);
+                var address = data.results[0];
+                console.log(data);
+                console.log(address);
+                $scope.locationofuser = data.results[6].address_components[1].long_name + ", " + data.results[9].address_components[0].short_name;
+                console.log($scope.locationofuser);
+                //            document.write(address.formatted_address);
+            }
+        };
+        request.send();
+    };
 })
 
 .controller('PlaylistCtrl', function ($scope, $stateParams) {});
